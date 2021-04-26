7d6ebba7fec598d0c7aa0a0e73280e69a6261e6d
Jelena Mihajlovic <starglitz47@gmail.com>
2021-04-26 21:28:56 +0200

    initial commit - initialized spring boot & react

---
+++ src/main/java/com/ftn/osa/OsaApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.osa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsaApplication.class, args);
+	}
+
+}
