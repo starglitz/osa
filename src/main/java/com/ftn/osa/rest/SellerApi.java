d6ff69588c5df4d23f0f6f28f30107c6778b5d48
Jelena Mihajlovic <starglitz47@gmail.com>
2021-04-28 17:39:36 +0200

    added some security changes

--- src/main/java/com/ftn/osa/rest/SellerApi.java
+++ src/main/java/com/ftn/osa/rest/SellerApi.java
@@ -2,6 +2,7 @@ package com.ftn.osa.rest;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sellers")
 public interface SellerApi {
 
+    @PreAuthorize("hasAnyRole('SELLER', 'ADMIN', 'CUSTOMER')")
     @GetMapping(value = "/all",
             produces = {MediaType.APPLICATION_JSON_VALUE})
     ResponseEntity getAllSellers();
